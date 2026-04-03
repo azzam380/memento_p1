77c18b10efebe63604328dc2bd7d18cb2b78c13f
Muahmmad Azzam <muh.azam0509@gmail.com>
2026-04-03 20:47:40 +0700

    Tahap Pengembangan

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -240,7 +240,6 @@ onMounted(() => {
         <h2 class="tts-level-title">{{ activeGame.title }} - {{ activeGame.difficulty }}</h2>
         <button class="finish-game-btn" @click="finishGame">
           Finish Game
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
         </button>
       </div>
 
